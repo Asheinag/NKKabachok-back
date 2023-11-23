65052e62aa54f492c3aafcc64a9e4949dfc4d007
Asheinag <asheinag@gmail.com>
2023-11-23 17:18:57 +0600

    added new routers

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ app = FastAPI(
     title='НК Кабачок'
 )
 
+
 @app.get("/")
 def index():
     return "Привет Кабачанцы!"
+
+
+@app.get("/news/")
+def news():
+    return "Тут будут новости"
+
+
+@app.get("/about/")
+def about():
+    return "Тут будут \"О нас\""
